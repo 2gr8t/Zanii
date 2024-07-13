e74308e79207adcd1c0ab08f4d9e4de951a3f785
2gr8t <info@2gr8tmedia.co.za>
2024-07-13 08:51:15 +0100

    Add files via upload

---
+++ styles.css
@@ -0,0 +1,76 @@
+body {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    color: #fff;
+    background: #000;
+}
+
+header {
+    background: #000;
+    padding: 10px 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+header .logo {
+    height: 50px;
+}
+
+header nav ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+}
+
+header nav ul li {
+    margin: 0 15px;
+}
+
+header nav ul li a {
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+section {
+    padding: 50px;
+    text-align: center;
+}
+
+#home {
+    background: #000;
+}
+
+#about, #contact, #social, #order {
+    background: #111;
+}
+
+button {
+    background: #fff;
+    color: #000;
+    border: 2px solid #fff;
+    padding: 10px 20px;
+    cursor: pointer;
+    font-size: 16px;
+    transition: all 0.3s ease;
+}
+
+button:hover {
+    background: #000;
+    color: #fff;
+    border: 2px solid #fff;
+}
+
+footer {
+    background: #000;
+    color: white;
+    text-align: center;
+    padding: 10px 0;
+}
+
+footer p {
+    margin: 0;
+}
